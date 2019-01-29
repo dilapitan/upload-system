94ddd28c90bbdc8b7544c8bedf9ce43e68eb6eb3
dom <domlapitan@gmail.com>
2019-01-29 19:30:03 +0800

    Create server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const config = require('./config/config')
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(config.port)
+console.log(`Server listening to port ${config.port}`)
